81471a6d1b00ef823bc06f2fea33f0780ef41d5d
charliechoong <e0202817@u.nus.edu>
2021-04-30 19:01:37 +0800

    Delete migrations folder

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-
+export AUTH0_DOMAIN="charlieagency.us.auth0.com"
+export API_AUDIENCE="jynn"
+export ALGORITHMS=["RS256"]
+#export DATABASE_URL="postgres://lubwuhvrzabxug:805ade3250caf26c33a71988d79fcdcbd53062ab19428f116a8aefe6203f0d53@ec2-3-212-75-25.compute-1.amazonaws.com:5432/d80su0jl0eroi5"
