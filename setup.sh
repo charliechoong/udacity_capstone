e82e83866ec1769ae48c92b1091890123fd6b744
charliechoong <e0202817@u.nus.edu>
2021-04-30 22:23:21 +0800

    Export client id and secret

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ export AUTH0_DOMAIN="charlieagency.us.auth0.com"
 export API_AUDIENCE="jynn"
 export ALGORITHMS=["RS256"]
 #export DATABASE_URL="postgres://lubwuhvrzabxug:805ade3250caf26c33a71988d79fcdcbd53062ab19428f116a8aefe6203f0d53@ec2-3-212-75-25.compute-1.amazonaws.com:5432/d80su0jl0eroi5"
+export CLIENT_SECRET="GsNrFem9yQUzGTAb_lnMm28QAMMBt5zjAeziGhgu9hOmob0106GFl9f8AwtNckkA"
+export CLIENT_ID="LHaMGrNNau9BCTir17GPC0fbb2JVtmKn"
